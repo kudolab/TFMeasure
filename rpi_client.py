71507c49db8d42536b4d1d011f1ba785b9e9a57e
tetsuzawa <tetsu.takizawa5@gmail.com>
2021-06-02 17:19:47 +0900

    fix client

--- rpi_client.py
+++ rpi_client.py
@@ -2,18 +2,25 @@ import json
 import sys
 
 import requests
+from requests.exceptions import Timeout
 
 BASE_ORIGIN = "http://172.24.176.169"
 
 
 def is_healthy():
-    url = "/health"
-    response = requests.get(url)
+    print("health check", file=sys.stderr)
+    url = BASE_ORIGIN+"/health"
+    try:
+        response = requests.get(url, timeout=5.0)
+    except Timeout:
+        print("requet timeout", file=sys.stderr)
+        return False
     return response.status_code == requests.codes.OK
 
 
 def put_speaker_num(speaker_num: int):
-    URL = "http://172.24.176.51/speaker"
+    print("put_speaker_num", file=sys.stderr)
+    URL = BASE_ORIGIN+"/speaker"
     speaker_data = {
         "speaker_num": speaker_num
     }
@@ -21,7 +28,11 @@ def put_speaker_num(speaker_num: int):
     headers = {"Content-Type": "application/json"}
     speaker_json = json.dumps(speaker_data).encode("utf-8")
 
-    response = requests.put(URL, data=speaker_json, headers=headers)
+    try:
+        response = requests.put(URL, data=speaker_json, headers=headers, timeout=5.0)
+    except Timeout:
+        print("requet timeout", file=sys.stderr)
+        return
     response.raise_for_status()
     responce_data = response.json()
     print(responce_data, file=sys.stderr)

f9b5ffb553c4b6774d30b4547d9b0ffe58126a32
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-06-02 14:26:06 +0900

    format

--- TFMeasure.py
+++ TFMeasure.py
@@ -11,24 +11,29 @@
 # ######################################################################
 
 
-import sys, os, re
-import numpy as np
+import os
+import re
+import sys
+from datetime import datetime
+
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import *
+import numpy as np
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from PyQt5.QtWidgets import *
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
-from datetime import datetime
-import measure, cpyconv
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) #このスクリプトのパス
+import measure
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))  # このスクリプトのパス
+
 
 class Window(QDialog):
     def __init__(self, parent=None):
-        super().__init__(parent)   
+        super().__init__(parent)
 
-        #測定データ保存先
+        # 測定データ保存先
         self.label_Save = QLabel('Save Directory:', self)
         self.edit_Save = QLineEdit(self)
         self.edit_Save.setFocusPolicy(Qt.ClickFocus)
@@ -37,94 +42,99 @@ class Window(QDialog):
         self.btn_Save.setStyleSheet('background-color:white; color:black;')
         self.btn_Save.clicked.connect(self.showDialog)
 
-        #測定モードの選択
+        # 測定モードの選択
         self.label_Mode = QLabel('Mode:', self)
         self.combo_Mode = QComboBox(self)
         self.combo_Mode.addItems(['RSTF', 'SSTF', 'LSTF', 'Mic_Ajust'])
 
-        #ターゲット
+        # ターゲット
         self.label_Output = QLabel('Target:', self)
         self.combo_Output = QComboBox(self)
         self.combo_Output.addItem("Headphones")
 
-        #被験者記入欄
+        # 被験者記入欄
         self.label_Sub = QLabel('subject:', self)
         self.edit_Sub = QLineEdit(self)
 
-        #同期化算数記入欄
+        # 同期化算数記入欄
         self.label_averaging_time = QLabel('Avaraging Times:', self)
         self.edit_averaging_time = QLineEdit(self)
         self.edit_averaging_time.setText("10")
 
-        #測定開始ボタン
+        # 測定開始ボタン
         self.btn_Start = QPushButton('START', self)
         self.btn_Start.setStyleSheet('background-color:cyan; color:black;')
         self.btn_Start.setFont(QFont("", 40))
-        
-        #イメージ
+
+        # イメージ
         self.image1 = QLabel(self)
         self.image1.setScaledContents(True)
-        self.image1.setPixmap(QPixmap(SCRIPT_DIR+"/.texture/RSTF.png"))
+        self.image1.setPixmap(QPixmap(SCRIPT_DIR + "/.texture/RSTF.png"))
 
-        #プロット切り替えボタン
+        # プロット切り替えボタン
         self.btn_pltChange = QPushButton('↔', self)
         self.btn_pltChange.setStyleSheet('background-color:white; color:black;')
         self.btn_pltChange.setFont(QFont("", 28))
         self.btn_pltChange.setHidden(True)
-        self.btn_pltChange.clicked.connect(self.plotChange) 
+        self.btn_pltChange.clicked.connect(self.plotChange)
 
-        #ログ
+        # ログ
         self.label_log = QLabel('Log:', self)
         self.te_log = QTextEdit(self)
-        self.te_log.setReadOnly(True) 
+        self.te_log.setReadOnly(True)
         self.te_log.setStyleSheet('background-color:black;')
         self.te_log.setTextColor(QColor(0, 255, 0))
         self.te_log.setFontPointSize(16)
 
-        #プロットウィンドウ
+        # プロットウィンドウ
         self.figure1 = plt.figure(figsize=(9, 5), dpi=60)
         self.axes1 = self.figure1.add_subplot(111)
         self.axes1.tick_params(labelsize=15)
         self.canvas1 = FigureCanvas(self.figure1)
         self.canvas1.setParent(self)
-        
-        #プロットウィンドウ2
+
+        # プロットウィンドウ2
         self.figure2 = plt.figure(figsize=(9, 5), dpi=60)
         self.axes2 = self.figure2.add_subplot(111)
         self.axes2.tick_params(labelsize=15)
         self.canvas2 = FigureCanvas(self.figure2)
         self.canvas2.setParent(self)
 
-        #ナビゲーションツール
+        # ナビゲーションツール
         self.toolbar1 = NavigationToolbar(self.canvas1, self)
         self.toolbar1.setGeometry(800, 80, 20, 30)
         self.toolbar2 = NavigationToolbar(self.canvas2, self)
         self.toolbar2.setGeometry(800, 390, 20, 30)
 
         self.btn_Start.clicked.connect(self.measure)
-        #ウィジェットイベント
+        # ウィジェットイベント
         self.combo_Mode.currentIndexChanged.connect(self.wiget_setting)
 
         self.wigets_layout()
 
     def showDialog(self):
         frame = QFileDialog.getExistingDirectory(self)
-        if frame != "": self.edit_Save.setText(frame+'/') 
+        if frame != "": self.edit_Save.setText(frame + '/')
 
-    #各ウィジェットの配置
+        # 各ウィジェットの配置
+
     def wigets_layout(self):
-        x1=20; y1=350;y2=380
-        x2=150;y3=450;y4=480
+        x1 = 20;
+        y1 = 350;
+        y2 = 380
+        x2 = 150;
+        y3 = 450;
+        y4 = 480
 
-        self.setGeometry(300,300,900,800) #ウィンドウサイズ
+        self.setGeometry(300, 300, 900, 800)  # ウィンドウサイズ
         self.image1.setGeometry(20, 60, 250, 250)
-        self.label_Save.move(x1, 10) 
+        self.label_Save.move(x1, 10)
         self.edit_Save.setGeometry(130, 10, 650, 20)
         self.btn_Save.setGeometry(800, 10, 80, 20)
-        self.label_Mode.move(x1, y1) 
-        self.combo_Mode.setGeometry(x1-3, y2, 120, 30)
+        self.label_Mode.move(x1, y1)
+        self.combo_Mode.setGeometry(x1 - 3, y2, 120, 30)
         self.label_Output.move(x2, y1)
-        self.combo_Output.setGeometry(x2-3, y2, 140, 30)
+        self.combo_Output.setGeometry(x2 - 3, y2, 140, 30)
         self.label_Sub.move(x1, y3)
         self.edit_Sub.setGeometry(x1, y4, 100, 20)
         self.label_averaging_time.move(x2, y3)
@@ -136,49 +146,50 @@ class Window(QDialog):
         self.canvas2.move(300, 360)
         self.btn_pltChange.setGeometry(860, 55, 30, 30)
 
-    #測定モードごとのウィジェットの設定
+    # 測定モードごとのウィジェットの設定
     def wiget_setting(self):
         self.combo_Output.clear()
         self.edit_Sub.setReadOnly(True)
 
-        #RSTF
+        # RSTF
         if self.combo_Mode.currentIndex() is 0:
             self.combo_Output.addItem("Headphones")
             self.edit_Sub.setReadOnly(False)
-            self.edit_averaging_time.setText("10") 
-            self.image1.setPixmap(QPixmap(SCRIPT_DIR+"/.texture/RSTF.png"))
-
-
-        #SSTF
-        elif self.combo_Mode.currentIndex() is 1:
-            self.combo_Output.addItems(['angle: 0-85', 'angle: 90-175', 'angle: 180-265', 'angle: 270-355', 'ITD_Check'])   
-            self.edit_Sub.setReadOnly(False) 
             self.edit_averaging_time.setText("10")
-            self.image1.setPixmap(QPixmap(SCRIPT_DIR+"/.texture/SSTF.png"))
+            self.image1.setPixmap(QPixmap(SCRIPT_DIR + "/.texture/RSTF.png"))
 
-        #LSTF
+
+        # SSTF
+        elif self.combo_Mode.currentIndex() is 1:
+            self.combo_Output.addItems(
+                ['angle: 0-85', 'angle: 90-175', 'angle: 180-265', 'angle: 270-355', 'ITD_Check'])
+            self.edit_Sub.setReadOnly(False)
+            self.edit_averaging_time.setText("10")
+            self.image1.setPixmap(QPixmap(SCRIPT_DIR + "/.texture/SSTF.png"))
+
+        # LSTF
         elif self.combo_Mode.currentIndex() is 2:
             self.combo_Output.addItems('Speaker No.' + str(n) for n in range(1, 19))
             self.edit_Sub.clear()
             self.edit_averaging_time.setText("10")
-            self.image1.setPixmap(QPixmap(SCRIPT_DIR+"/.texture/LSTF.png"))
+            self.image1.setPixmap(QPixmap(SCRIPT_DIR + "/.texture/LSTF.png"))
 
-        #MicAjust
+        # MicAjust
         elif self.combo_Mode.currentIndex() is 3:
             self.combo_Output.addItem("Speaker No.1")
             self.edit_Sub.clear()
-            self.image1.setPixmap(QPixmap(SCRIPT_DIR+""))
+            self.image1.setPixmap(QPixmap(SCRIPT_DIR + ""))
 
-    #測定
+    # 測定
     def measure(self):
         self.averaging_times = self.edit_averaging_time.text()
         self.speaker_index = self.combo_Output.currentIndex()
         self.subject = self.edit_Sub.text()
         self.outdir = self.edit_Save.text() + "/" + self.subject
-        self.Reverse = False #プロット切り替えスイッチ
+        self.Reverse = False  # プロット切り替えスイッチ
         self.btn_pltChange.setHidden(False)
 
-        #RSTF
+        # RSTF
         if self.combo_Mode.currentIndex() is 0:
             if self.averaging_times is "":
                 QMessageBox.warning(self, "Message", u"SANnum is invalid or empty")
@@ -188,10 +199,10 @@ class Window(QDialog):
                 return
             measure.RSTF(self.subject, self.averaging_times, 1, 255, 3801, 4823, self.outdir)
             self.te_log.append('cinv_cRSTF_L.DDB and cinv_cRSTF_R.DDB are measured. ('
-                                + datetime.now().strftime("%H:%M:%S")  + ')')
+                               + datetime.now().strftime("%H:%M:%S") + ')')
             self.plotChange()
 
-        #SSTF
+        # SSTF
         elif self.combo_Mode.currentIndex() is 1:
             self.btn_pltChange.setHidden(True)
             if os.path.exists("/Volumes/share/angle") is False:
@@ -203,25 +214,26 @@ class Window(QDialog):
             if self.subject is "":
                 QMessageBox.warning(self, "Message", u"subject Name is empty")
                 return
-            if self.speaker_index is 4: 
+            if self.speaker_index is 4:
                 with open('/Volumes/share/angle', 'w') as select: select.write("1")
                 measure.SSTF(self.subject, self.averaging_times, 'check', 150, 405, self.outdir)
                 self.plot(self.outdir + '/SSTF/cSSTF_check_L.DDB'
-                        , self.outdir + '/SSTF/cSSTF_check_R.DDB')        #/SSTF/cSSTF_000_R.DDBから/SSTF/cSSTF_check_R.DDBに名称変更
+                          ,
+                          self.outdir + '/SSTF/cSSTF_check_R.DDB')  # /SSTF/cSSTF_000_R.DDBから/SSTF/cSSTF_check_R.DDBに名称変更
                 return
 
             for n in range(18):
                 angle = self.speaker_index * 90 + n * 5
-                with open('/Volumes/share/angle', 'w') as select: select.write(str(n+1))
+                with open('/Volumes/share/angle', 'w') as select: select.write(str(n + 1))
                 measure.SSTF(self.subject, self.averaging_times, angle, 150, 405, self.outdir)
-                self.te_log.append('SSTF_' + str(angle) + '_L.DDB and SSTF_' 
-                                + str(angle) + '_R.DDB are measured. ('+ datetime.now().strftime("%H:%M:%S")  + ')')
+                self.te_log.append('SSTF_' + str(angle) + '_L.DDB and SSTF_'
+                                   + str(angle) + '_R.DDB are measured. (' + datetime.now().strftime("%H:%M:%S") + ')')
                 self.plot(self.outdir + '/SSTF/cSSTF_' + str(angle) + '_L.DDB'
-                        , self.outdir + '/SSTF/cSSTF_' + str(angle) + '_R.DDB')
+                          , self.outdir + '/SSTF/cSSTF_' + str(angle) + '_R.DDB')
                 self.canvas1.flush_events()
                 self.canvas2.flush_events()
 
-        #LSTF
+        # LSTF
         elif self.combo_Mode.currentIndex() is 2:
             if os.path.exists("/Volumes/share/angle") is False:
                 QMessageBox.warning(self, "Message", u"Speaker selector is not connecting.")
@@ -230,33 +242,34 @@ class Window(QDialog):
                 QMessageBox.warning(self, "Message", u"SANnum is invalid or empty")
                 return
             with open('/Volumes/share/angle', 'w') as select:
-                        select.write(str(self.speaker_index+1))
-            measure.LSTF(self.speaker_index+1, self.averaging_times, 150, 405, 3800, 4823, self.edit_Save.text())
-            self.te_log.append('/LSTF_' + str(self.speaker_index+1) + '.DDB is measured. ('
-                                + datetime.now().strftime("%H:%M:%S")  + ')')
+                select.write(str(self.speaker_index + 1))
+            measure.LSTF(self.speaker_index + 1, self.averaging_times, 150, 405, 3800, 4823, self.edit_Save.text())
+            self.te_log.append('/LSTF_' + str(self.speaker_index + 1) + '.DDB is measured. ('
+                               + datetime.now().strftime("%H:%M:%S") + ')')
             self.plotChange()
 
-        #MicAjust
+        # MicAjust
         elif self.combo_Mode.currentIndex() is 3:
-            with open('/Volumes/share/angle', 'w') as select: select.write("1")
+            with open('/Volumes/share/angle', 'w') as select:
+                select.write("1")
             measure.mic_ajust()
-            self.te_log.append('rec_L.DDB and rec_R.DDB are measured. ('+ datetime.now().strftime("%H:%M:%S")  + ')')
+            self.te_log.append('rec_L.DDB and rec_R.DDB are measured. (' + datetime.now().strftime("%H:%M:%S") + ')')
             self.plotChange()
-        
+
         # elif self.combo_Mode.currentIndex() is 4:
         #     cpyconv.closedloop()
         #     with open(SCRIPT_DIR+"/DOUKI_START", 'r') as douki_start: iodelay = douki_start.read()
         #     QMessageBox.about(self, "Message", "The I/O delay is "+iodelay+" sample")
 
-    #データのプロット
-    def plot(self, file_L, file_R): 
+    # データのプロット
+    def plot(self, file_L, file_R):
         data_bin = open(file_L, 'rb').read()
-        data = np.fromstring(data_bin,dtype=np.float64)
+        data = np.fromstring(data_bin, dtype=np.float64)
         if file_R != None:
             data_bin2 = open(file_R, 'rb').read()
-            data2 = np.fromstring(data_bin2,dtype=np.float64)
+            data2 = np.fromstring(data_bin2, dtype=np.float64)
 
-        #プロット1
+        # プロット1
         self.axes1.clear()
         self.axes1.set_title('Impulse Response', fontsize=15)
         self.axes1.set_xlabel("Sample", fontsize=15)
@@ -264,10 +277,10 @@ class Window(QDialog):
         self.axes1.plot(data, '-', label=re.search("(.*)/(.*)", file_L).group(2))
         if file_R != None:
             self.axes1.plot(data2, '-', label=re.search("(.*)/(.*)", file_R).group(2))
-        self.axes1.legend(bbox_to_anchor=(0., 1.02, 1., .102),borderaxespad=-0.2)
+        self.axes1.legend(bbox_to_anchor=(0., 1.02, 1., .102), borderaxespad=-0.2)
         self.canvas1.draw()
 
-        #プロット2
+        # プロット2
         self.axes2.clear()
         self.axes2.set_title('Frequency Characteristic', fontsize=15)
         self.axes2.set_xlabel("Frequency [Hz]", fontsize=15)
@@ -275,52 +288,51 @@ class Window(QDialog):
         self.axes2.set_xlim(100, 24000)
         self.axes2.set_xscale('log')
         N = 255
-        x = np.fft.fftfreq(N*2,d=1.0/48000)*2
+        x = np.fft.fftfreq(N * 2, d=1.0 / 48000) * 2
         freq = np.fft.fft(data[0:N])
-        data_amplitude = [np.sqrt(c.real ** 2  + c.imag ** 2 ) for c in freq]
+        data_amplitude = [np.sqrt(c.real ** 2 + c.imag ** 2) for c in freq]
         data_decibel = 10.0 * np.log10(data_amplitude)
         self.axes2.plot(x[0:N], data_decibel, '-', label=re.search("(.*)/(.*)", file_L).group(2))
 
         if file_R != None:
             freq2 = np.fft.fft(data2[0:N])
-            data_amplitude2 = [np.sqrt(c.real ** 2  + c.imag ** 2 ) for c in freq2]
+            data_amplitude2 = [np.sqrt(c.real ** 2 + c.imag ** 2) for c in freq2]
             data_decibel2 = 10.0 * np.log10(data_amplitude2)
             self.axes2.plot(x[0:N], data_decibel2, '-', label=re.search("(.*)/(.*)", file_R).group(2))
-        
-        self.axes2.legend(bbox_to_anchor=(0., 1.02, 1., .102),borderaxespad=-0.2)
+
+        self.axes2.legend(bbox_to_anchor=(0., 1.02, 1., .102), borderaxespad=-0.2)
         self.canvas2.draw()
 
     def plotChange(self):
-        if self.Reverse is False: 
-            #RSTF
+        if self.Reverse is False:
+            # RSTF
             if self.combo_Mode.currentIndex() is 0:
                 self.plot(self.outdir + '/RSTF/cRSTF_L.DDB'
-                        , self.outdir + '/RSTF/cRSTF_R.DDB')
-            #LSTF
+                          , self.outdir + '/RSTF/cRSTF_R.DDB')
+            # LSTF
             elif self.combo_Mode.currentIndex() is 2:
-                self.plot(self.edit_Save.text() + '/LSTF/cLSTF_' + str(self.speaker_index+1) + '.DDB', None)
-            #MicAjust
+                self.plot(self.edit_Save.text() + '/LSTF/cLSTF_' + str(self.speaker_index + 1) + '.DDB', None)
+            # MicAjust
             elif self.combo_Mode.currentIndex() is 3:
                 self.plot('/tmp/rec_L.DDB', '/tmp/rec_R.DDB')
             self.Reverse = True
 
         elif self.Reverse is True:
-            #RSTF
+            # RSTF
             if self.combo_Mode.currentIndex() is 0:
                 self.plot(self.outdir + '/RSTF/cinv_cRSTF_L.DDB'
-                        , self.outdir + '/RSTF/cinv_cRSTF_R.DDB')
-            #LSTF
+                          , self.outdir + '/RSTF/cinv_cRSTF_R.DDB')
+            # LSTF
             elif self.combo_Mode.currentIndex() is 2:
-                self.plot(self.edit_Save.text() + '/LSTF/cinv_cLSTF_' + str(self.speaker_index+1) + '.DDB', None)
-            #MicAjust
+                self.plot(self.edit_Save.text() + '/LSTF/cinv_cLSTF_' + str(self.speaker_index + 1) + '.DDB', None)
+            # MicAjust
             elif self.combo_Mode.currentIndex() is 3:
                 self.plot('/tmp/rec_R.DDB', '/tmp/rec_L.DDB')
             self.Reverse = False
 
-
-    #ウィンドウ内の座標を表示
+    # ウィンドウ内の座標を表示
     def mousePressEvent(self, event):
-        print('x='+ str(event.x()) + ', y=' + str(event.y()))
+        print('x=' + str(event.x()) + ', y=' + str(event.y()))
 
 
 if __name__ == '__main__':
@@ -331,4 +343,3 @@ if __name__ == '__main__':
     main.show()
 
     sys.exit(app.exec_())
-
--- measure.py
+++ measure.py
@@ -10,25 +10,29 @@
 # SSTF内にif文追加(53行), ITD_checkの分岐@2019.5.23
 # ######################################################################
 
-from cpyconv import playrec_mono, playrec_2ch, averaging, cross_correlation, unbias, trim, inverse, speach
 import os
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) #このスクリプトのパス
-band_path_filter = SCRIPT_DIR+"/BPFBPFBPF100-13k_48k_0-255.DDB"
+from cpyconv import playrec_mono, playrec_2ch, averaging, cross_correlation, unbias, trim, inverse, speach
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))  # このスクリプトのパス
+band_path_filter = SCRIPT_DIR + "/BPFBPFBPF100-13k_48k_0-255.DDB"
+
 
 def LSTF(num, averaging_times, trim_imp_start, trim_imp_end, trim_inv_start, trim_inv_end, outdir):
-    outdir=outdir+"/LSTF"
+    outdir = outdir + "/LSTF"
     if not os.path.exists(outdir): os.makedirs(outdir)
     playrec_mono(averaging_times, "/tmp/rec.DSB")
     averaging("/tmp/rec.DSB", averaging_times, "/tmp/tmp.DSB")
-    cross_correlation("/tmp/tmp.DSB", outdir+"/LSTF_"+str(num)+".DDA")
-    unbias(outdir+"/LSTF_"+str(num)+".DDA")
-    trim(outdir+"/LSTF_"+str(num)+".DDA", trim_imp_start, trim_imp_end, outdir+"/cLSTF_"+str(num)+".DDB")
-    inverse(outdir+"/cLSTF_"+str(num)+".DDB", band_path_filter, outdir+"/inv_cLSTF_"+str(num)+".DDB", 4096)
-    trim(outdir+"/inv_cLSTF_"+str(num)+".DDB", trim_inv_start, trim_inv_end, outdir+"/cinv_cLSTF_"+str(num)+".DDB")
+    cross_correlation("/tmp/tmp.DSB", outdir + "/LSTF_" + str(num) + ".DDA")
+    unbias(outdir + "/LSTF_" + str(num) + ".DDA")
+    trim(outdir + "/LSTF_" + str(num) + ".DDA", trim_imp_start, trim_imp_end, outdir + "/cLSTF_" + str(num) + ".DDB")
+    inverse(outdir + "/cLSTF_" + str(num) + ".DDB", band_path_filter, outdir + "/inv_cLSTF_" + str(num) + ".DDB", 4096)
+    trim(outdir + "/inv_cLSTF_" + str(num) + ".DDB", trim_inv_start, trim_inv_end,
+         outdir + "/cinv_cLSTF_" + str(num) + ".DDB")
+
 
 def RSTF(subject, averaging_times, trim_imp_start, trim_imp_end, trim_inv_start, trim_inv_end, subdir):
-    outdir=subdir+"/RSTF"
+    outdir = subdir + "/RSTF"
     for dir in [subdir, outdir]:
         if not os.path.exists(dir): os.makedirs(dir)
     playrec_2ch(averaging_times, "/tmp/rec.DSB")
@@ -36,40 +40,44 @@ def RSTF(subject, averaging_times, trim_imp_start, trim_imp_end, trim_inv_start,
     averaging("/tmp/rec_1.DSB", averaging_times, "/tmp/tmp_L.DSB")
     averaging("/tmp/rec_2.DSB", averaging_times, "/tmp/tmp_R.DSB")
     for LR in ['L', 'R']:
-        cross_correlation("/tmp/tmp_"+LR+".DSB", outdir+"/RSTF_"+LR+".DDA")
-        unbias(outdir+"/RSTF_"+LR+".DDA")
-        trim(outdir+"/RSTF_"+LR+".DDA", trim_imp_start, trim_imp_end, outdir+"/cRSTF_"+LR+".DDB")
-        inverse(outdir+"/cRSTF_"+LR+".DDB", band_path_filter, outdir+"/inv_cRSTF_"+LR+".DDB", 4096)
-        trim(outdir+"/inv_cRSTF_"+LR+".DDB", trim_inv_start, trim_inv_end, outdir+"/cinv_cRSTF_"+LR+".DDB")
+        cross_correlation("/tmp/tmp_" + LR + ".DSB", outdir + "/RSTF_" + LR + ".DDA")
+        unbias(outdir + "/RSTF_" + LR + ".DDA")
+        trim(outdir + "/RSTF_" + LR + ".DDA", trim_imp_start, trim_imp_end, outdir + "/cRSTF_" + LR + ".DDB")
+        inverse(outdir + "/cRSTF_" + LR + ".DDB", band_path_filter, outdir + "/inv_cRSTF_" + LR + ".DDB", 4096)
+        trim(outdir + "/inv_cRSTF_" + LR + ".DDB", trim_inv_start, trim_inv_end, outdir + "/cinv_cRSTF_" + LR + ".DDB")
+
 
 def SSTF(subject, averaging_times, angle, trim_imp_start, trim_imp_end, subdir):
-    outdir=subdir+"/SSTF"
+    outdir = subdir + "/SSTF"
     for dir in [subdir, outdir]:
         if not os.path.exists(dir): os.makedirs(dir)
     playrec_2ch(averaging_times, "/tmp/rec.DSB")
     speach(2, "/tmp/rec.DSB")
     averaging("/tmp/rec_1.DSB", averaging_times, "/tmp/tmp_L.DSB")
     averaging("/tmp/rec_2.DSB", averaging_times, "/tmp/tmp_R.DSB")
-    #追加したif文
-    if str(angle)=="check":                                                                      #追加した行
+    # 追加したif文
+    if str(angle) == "check":  # 追加した行
         for LR in ['L', 'R']:
-            cross_correlation("/tmp/tmp_"+LR+".DSB", outdir+"/SSTF_check_"+LR+".DDA")
-            unbias(outdir+"/SSTF_check_"+LR+".DDA")
-            trim(outdir+"/SSTF_check_"+LR+".DDA", trim_imp_start, trim_imp_end, outdir+"/cSSTF_check_"+LR+".DDB")
+            cross_correlation("/tmp/tmp_" + LR + ".DSB", outdir + "/SSTF_check_" + LR + ".DDA")
+            unbias(outdir + "/SSTF_check_" + LR + ".DDA")
+            trim(outdir + "/SSTF_check_" + LR + ".DDA", trim_imp_start, trim_imp_end,
+                 outdir + "/cSSTF_check_" + LR + ".DDB")
     else:
         for LR in ['L', 'R']:
-            cross_correlation("/tmp/tmp_"+LR+".DSB", outdir+"/SSTF_"+str(angle)+"_"+LR+".DDA")
-            unbias(outdir+"/SSTF_"+str(angle)+"_"+LR+".DDA")
-            trim(outdir+"/SSTF_"+str(angle)+"_"+LR+".DDA", trim_imp_start, trim_imp_end, outdir+"/cSSTF_"+str(angle)+"_"+LR+".DDB")
+            cross_correlation("/tmp/tmp_" + LR + ".DSB", outdir + "/SSTF_" + str(angle) + "_" + LR + ".DDA")
+            unbias(outdir + "/SSTF_" + str(angle) + "_" + LR + ".DDA")
+            trim(outdir + "/SSTF_" + str(angle) + "_" + LR + ".DDA", trim_imp_start, trim_imp_end,
+                 outdir + "/cSSTF_" + str(angle) + "_" + LR + ".DDB")
     # ######################################################################
-    #for LR in ['L', 'R']:
+    # for LR in ['L', 'R']:
     #    cross_correlation("/tmp/tmp_"+LR+".DSB", outdir+"/SSTF_"+str(angle)+"_"+LR+".DDA")
     #    unbias(outdir+"/SSTF_"+str(angle)+"_"+LR+".DDA")
     #    trim(outdir+"/SSTF_"+str(angle)+"_"+LR+".DDA", trim_imp_start, trim_imp_end, outdir+"/cSSTF_"+str(angle)+"_"+LR+".DDB")
     # ######################################################################
+
+
 def mic_ajust():
     playrec_2ch(5, "/tmp/rec.DSB")
     speach(2, "/tmp/rec.DSB")
     averaging("/tmp/rec_1.DSB", 5, "/tmp/rec_L.DDB")
     averaging("/tmp/rec_2.DSB", 5, "/tmp/rec_R.DDB")
-
